71c285753615b1640ca9042b4db5b4dbb90bb6a1
Jusca <jusca.mol@gmail.com>
2024-10-20 19:53:45 +0000

    fixes

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,4 +1,4 @@
-i-- creates a stored procedure ComputeAverageWeightedScoreForUser that
+-- creates a stored procedure ComputeAverageWeightedScoreForUser that
 -- computes and store the average weighted score for a student.
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER $$
