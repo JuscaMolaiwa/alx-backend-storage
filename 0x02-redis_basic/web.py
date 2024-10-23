d3c5f31a2e2c68ce03bc72852018c2ddec5f8c18
root <Jusca.mol@gmail.comgit config --global user.name Jusca Molaiwa>
2024-10-23 18:22:48 +0000

    fails

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,36 @@
 #!/usr/bin/env python3
-'''A module providing tools for caching HTTP requests and tracking access
-'''
+"""
+Caching request module
+"""
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
 
-redis_store = redis.Redis()
-'''A Redis instance used for caching request data and tracking access counts.
-'''
+def track_get_page(fn: Callable) -> Callable:
+    """ Decorator for get_page
+    """
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """ Wrapper that:
+            - check whether a url's data is cached
+            - tracks how many times get_page is called
+        """
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
+    return wrapper
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''A decorator that caches the result of an HTTP request and tracks
-    the number of times the request has been made for a given URL.
-    '''
-    @wraps(method)
-    def invoker(url: str) -> str:
-        '''Wraps the original function to add caching and request tracking
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
-
-
-@data_cacher
+@track_get_page
 def get_page(url: str) -> str:
-    '''Fetches the content of a webpage and caches the result.
-    '''
-    return requests.get(url).text
+    """ Makes a http request to a given endpoint
+    """
+    response = requests.get(url)
+    return response.text
